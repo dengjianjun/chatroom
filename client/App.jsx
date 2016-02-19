7ea8d7408e9754565ab35511d4fc11205df9a7fc
Peter Wang <happypeter1983@gmail.com>
2016-02-19 16:49:09 +0800

    响应式的导航栏

--- client/App.jsx
+++ client/App.jsx
@@ -1,8 +1,16 @@
+const { AppBar } = mui;
 App = React.createClass({
+  componentWillMount() {
+    let setNavBarState = function() {
+      this.setState({renderNavBar: document.body.clientWidth > 700})
+    }.bind(this);
+    setNavBarState();
+    window.onresize = setNavBarState;
+  },
 	render() {
 		return(
 			<div>
-        <NavBar />
+        {this.state.renderNavBar?<NavBar />:<AppBar />}
         <br />
         {this.props.children}
 			</div>

064b04f863d9917b8674a362a185a7d64bcb065c
Peter Wang <happypeter1983@gmail.com>
2016-02-19 16:28:57 +0800

    生命周期函数中操作State

--- client/NavBar.jsx
+++ client/NavBar.jsx
@@ -1,5 +1,25 @@
 const { Tabs, Tab } = mui;
 NavBar = React.createClass({
+  getInitialState() {
+    return {
+      value: '/home'
+    };
+  },
+  componentWillReceiveProps() {
+    this.setState({
+      value: this._getSelectedIndex()
+    });
+  },
+  componentWillMount() {
+    this.setState({
+      value: this._getSelectedIndex()
+    });
+  },
+  _getSelectedIndex() {
+    return this.context.router.isActive('/home')?'/home':
+           this.context.router.isActive('/signup')?'/signup':
+           this.context.router.isActive('/login')?'/login':'/home';
+  },
   contextTypes: {
     router: React.PropTypes.object.isRequired
   },
@@ -9,7 +29,7 @@ NavBar = React.createClass({
   render() {
     return (
       <div>
-        <Tabs onChange={this._handleChange}>
+        <Tabs onChange={this._handleChange} value={this.state.value}>
           <Tab label="Home" value="/home" />
           <Tab label="Sign Up" value="/signup" />
           <Tab label="Log In" value="/login" />
